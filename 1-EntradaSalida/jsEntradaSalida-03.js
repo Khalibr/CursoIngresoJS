d5e3be86422bc594f5003f71e1064f378d35fdbb
Khalibr <julianmedina219@gmail.com>
2022-01-31 18:03:24 -0300

    practicando el push git

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,15 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombre;
+	// "var nombre; " <- var = let (misma palabra designada)
+ 	// nombre=txtIdNombre.value; <- funciona pero no es la forma de hacerlo
+	
+	nombre = document.getElementById("txtIdNombre").value;
+
+	alert(nombre);
+
+	document.getElementById("txtIdNombre").value = ""; //-> esta linea sirve para una vez aceptado el alert deje la caja por default
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,22 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
-}
+	
+	//let datoingresado;
+	//datoingresado=txtIdNombre.value;
+	//datoingresado=prompt("Ingrese el dato");
+	//txtIdNombre.value=datoingresado;
+	
+	// reservo espacio en memoria para guardar el nombre del user
+	let nombre;
+	
+	// guardo en la var nombre el txt que escribio el user dentro del promp
+	nombre = prompt("Ingrese su nombre");
+
+	//copio el txt que user que uso dentro de promp para mostrarlo en el html
+	document.getElementById("txtIdNombre").value = nombre;
+
+
+}	
+	
 
